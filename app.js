27a5f66b0e2ad37322ec3b599bc16ce9f57e3e9d
alexjay1997 <aljayfauni@gmail.com>
2022-09-22 04:12:18 +0800

    added delete and insert function

--- app.js
+++ app.js
@@ -1,43 +1,69 @@
-const api_url = 
-      "http://localhost:3000/tests";
+// const api_url = 
+//       "http://localhost:3000/tests";
   
-// Defining async function
-async function getapi(url) {
+// // Defining async function
+// async function getapi(url) {
     
-    // Storing response
-    const response = await fetch(url);
+//     // Storing response
+//     const response = await fetch(url);
     
-    // Storing data in form of JSON
-    var data = await response.json();
-    console.log(data);
-    if (response) {
+//     // Storing data in form of JSON
+//     var data = await response.json();
+//     console.log(data);
+//     if (response) {
  
-    }
-    show(data);
-}
-// Calling that async function
-getapi(api_url);
+//     }
+//     show(data);
+// }
+// // Calling that async function
+// getapi(api_url);
   
 
-// Function to define innerHTML for HTML table
-function show(data) {
-    let tab =''; 
-        // `<tr>
-        //   <th>ID</th>
-        //   <th>Name</th>
-        //   <th>Age</th>
-        //   <th>Address</th>
-        //  </tr>`;
+// // Function to define innerHTML for HTML table
+// function show(data) {
+//     let tab =''; 
+//         // `<tr>
+//         //   <th>ID</th>
+//         //   <th>Name</th>
+//         //   <th>Age</th>
+//         //   <th>Address</th>
+//         //  </tr>`;
     
-    // Loop to access all rows 
-    for (let row of data) {
-        tab += `<tr> 
-    <td>${row.id} </td>
-    <td>${row.name}</td>
-    <td>${row.age}</td> 
-    <td>${row.address}</td>          
-</tr>`;
-    }
-    // Setting innerHTML as tab variable
-    document.getElementById("data").innerHTML = tab;
-}
+//     // Loop to access all rows 
+//     for (let row of data) {
+//         tab += `<tr> 
+//     <td>${row.id} </td>
+//     <td>${row.name}</td>
+//     <td>${row.age}</td> 
+//     <td>${row.address}</td>          
+// </tr>`;
+//     }
+//     // Setting innerHTML as tab variable
+//     document.getElementById("data").innerHTML = tab;
+// }
+
+
+fetch("http://localhost:3000/tests")
+            .then(res => res.json())
+            .then(data => {
+             console.log(data);
+             var outputdata="";
+                    
+     //loop start
+data.forEach((datas)=>{
+    var img = new Image();
+   // img.src = company.Images;
+    outputdata += `
+<tr>
+<td>${datas.id}</td>
+<td>${datas.name}</td>
+<td>${datas.age}</td>
+<td>${datas.address}</td>
+</tr>                 
+    `;                     
+//document.getElementById("data").append(img);
+
+})
+
+document.getElementById("data").innerHTML= outputdata;
+})
--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const mysql =  require('mysql');
 const express =  require('express');
+const cors = require('cors') //you need to `npm install cors` first
+const path = require('path');
+const router = express.Router();
 
 var app = express();
-
 const bodyparser = require('body-parser');
 
+app.use(cors()) //enable cors requests
 app.use(bodyparser.json());
 
 var mysqlConnection = mysql.createConnection(
     {
-
         host:'localhost',
         user:'root',
         password:'',
         port:'3307',//port is 3307 because i have also a wamp server installed in my computer! the default is 3306!
         database:'Nodejs_Mysql'
+     
     });
 
 mysqlConnection.connect((err)=>{
@@ -22,7 +25,6 @@ mysqlConnection.connect((err)=>{
     console.log('db connection success!');
     else
     console.log('db connection failed! \n Error:'+ JSON.stringify(err,undefined,2));
-
 }) ;  
 
 app.listen(3000,() =>console.log( ' Express server is runnig at port no : 3000 ' ) ) ;
@@ -41,7 +43,7 @@ app.get('/tests' ,( req ,res )=> {
 // select data by id
 app.get('/tests/:id' ,( req ,res )=> {
     mysqlConnection.query( ' SELECT * FROM test WHERE id = ?',[req.params.id] ,( err , rows , fields ) => {
-     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', '*');
      if(!err)
      res.send(rows);
      //console.log(rows);
@@ -49,3 +51,36 @@ app.get('/tests/:id' ,( req ,res )=> {
      console.log(err);
     })
  }); 
+ //insert new data
+ app.post('/tests',( req ,res )=> {
+    const name = req.body.name;
+    const address = req.body.address;
+    const age = req.body.age;
+    mysqlConnection.query("insert into test(name,address,age) values(?,?,?)",[name,address,age],(err,rows)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+     if(!err){
+        res.send('Insert Sucessfully!');
+    }
+     else{
+     console.log(err);
+     }
+    })
+ }); 
+// delete data by id
+app.delete('/tests/:id' ,( req ,res )=> {
+    mysqlConnection.query( ' DELETE FROM test WHERE id = ?',[req.params.id] ,( err , rows , fields ) => {
+    res.header('Access-Control-Allow-Origin', '*');
+     if(!err)
+     res.send("Deleted Successfully!");
+     else
+     console.log(err);
+    })
+ });
+
+
+ router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/views'+'/index.html'));
+
+  });
+  app.use('/', router);
+  
